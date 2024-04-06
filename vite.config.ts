189e8478308ed5c91684f8e79ea523d126d2c9e0
mytja <mitja@severkar.eu>
2024-04-06 16:07:41 +0200

    dockerize

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { replaceCodePlugin } from "vite-plugin-replace";
+import replace from "vite-plugin-filter-replace";
 
 const production = 'import.meta.env.PROD';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    replaceCodePlugin({
-      replacements: [
-        {
+    replace([
+      {
+        filter: "src/lib/consts.ts",
+        replace: {
           from: "isProduction",
           to: production,
         },
-      ],
-    }),
+      },
+    ]),
     svelte(),
   ],
 })
