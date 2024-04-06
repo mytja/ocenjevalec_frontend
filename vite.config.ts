963cf3b842a9322eb62095d755417cbc4beb0aaa
mytja <mitja@severkar.eu>
2024-04-06 18:16:47 +0200

    check for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import replace from "vite-plugin-filter-replace";
-
-const production = 'import.meta.env.PROD';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    replace([
-      {
-        filter: "src/lib/consts.ts",
-        replace: {
-          from: "isProduction",
-          to: production,
-        },
-      },
-    ]),
     svelte(),
   ],
 })
