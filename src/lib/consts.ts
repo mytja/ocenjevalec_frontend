5a2c4a300110d8b4c3bf85a1e049651ccef7019f
mytja <mitja@severkar.eu>
2024-04-28 21:34:32 +0200

    production URL for websockets

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,3 +1,3 @@
 // @ts-ignore
 export const production: boolean = import.meta.env.PROD;
-export let baseurl: string = (!production ? "http://127.0.0.1:8000" : "/api");
+export let baseurl: string = (!production ? "http://127.0.0.1:8000" : "https://ocenjevalec.beziapp.si/api");
